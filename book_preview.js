3a71a97d028c5898000fd7b7424384fc6855e3ee
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-03 00:19:49 +0200

    created a function for when an element is added to the page

--- book_preview.js
+++ book_preview.js
@@ -3,4 +3,10 @@ class BookPreview extends HTMLElement {
         super(); //to ensure that bookPreview inherits from HTML element
         this.attachShadow({ mode: 'open' }); //creating my shadow DOM
     }
+
+    // Called when element is added to the page
+    connectedCallback() {
+        this.render();
+        this.addClickListener();
+    }
 }
