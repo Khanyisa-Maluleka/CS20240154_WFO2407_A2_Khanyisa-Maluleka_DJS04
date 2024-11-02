18c7b8bdf32f81f0d978f863b9bb8a827d474b51
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-03 00:27:04 +0200

    Added an event listener

--- book_preview.js
+++ book_preview.js
@@ -10,10 +10,23 @@ class BookPreview extends HTMLElement {
         this.addClickListener();
     }
 
-    render() {
+    render() { //retrieves the book's attributes (ID, title, author, and image)
         const bookId = this.getAttribute('book-id');
         const title = this.getAttribute('title');
         const author = this.getAttribute('author');
         const image = this.getAttribute('image');
+    }
+
+    addClickListener() {
+        const button = this.shadowRoot.querySelector('.preview');
+        button.addEventListener('click', () => {
+            // Create a custom event when book is clicked
+            const event = new CustomEvent('book-selected', {
+                bubbles: true,
+                composed: true,
+                detail: { bookId: this.getAttribute('book-id') }
+            });
+            this.dispatchEvent(event);
+        });
     }
 }
