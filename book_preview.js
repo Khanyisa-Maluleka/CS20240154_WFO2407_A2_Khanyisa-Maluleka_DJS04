7d0b6d2581804b831a41ec8f402e9eaf7e37a2b9
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-03 00:18:18 +0200

    created my shadow DOM

--- book_preview.js
+++ book_preview.js
@@ -1,5 +1,6 @@
 class BookPreview extends HTMLElement {
     constructor() {
         super(); //to ensure that bookPreview inherits from HTML element
+        this.attachShadow({ mode: 'open' }); //creating my shadow DOM
     }
 }
